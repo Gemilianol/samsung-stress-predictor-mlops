d5be56d43498768ae02bbc303c6559e5a5aa4b0c
Gemilianol <glabonia@mail.utdt.edu>
2025-08-14 20:04:57 -0400

    empty config added

--- backend/src/components/config.py
+++ backend/src/components/config.py
@@ -7,4 +7,8 @@ Replace with real configuration in development/production environments.
 # Example dummy vars to avoid attribute errors in imports
 DATA_PATH = ""
 MODEL_PATH = ""
-DATA_SOURCES = {}
+DATA_SOURCES = {    
+                'stress': {
+    },
+    'heart_rate': {}
+}
