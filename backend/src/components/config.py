375810554fb7318138719486238cfbeb31fb980c
Gemilianol <glabonia@mail.utdt.edu>
2025-08-15 17:44:54 -0400

    another try for config

--- backend/src/components/config.py
+++ backend/src/components/config.py
@@ -7,8 +7,24 @@ Replace with real configuration in development/production environments.
 # Example dummy vars to avoid attribute errors in imports
 DATA_PATH = ""
 MODEL_PATH = ""
-DATA_SOURCES = {    
-                'stress': {
+DATA_SOURCES = {
+    'stress': {
+        'file_path': '',
+          'cols_to_keep': [],
+          'col_date': 'date',
+          'prefix': 'stress_'
     },
-    'heart_rate': {}
+    'heart_rate': {
+        'file_path': '',
+              'cols_to_keep':[],
+              'col_date': {'oem':'date',
+                           'mod': 'date'},
+              'prefix': 'heart_',
+              'data_type': 'heart',
+              'new_name_columns': ['date', 'heart_max_rate', 'heart_min_rate','heart_rate'],
+              
+    },
+    'data_transformation': {
+        'features_to_lag': ['heart_min_rate']
+    }
 }
